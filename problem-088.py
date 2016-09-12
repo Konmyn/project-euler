3f4dccf4947fbd926e95352642f2df41ca61e515
Ethan <konmyn@163.com>
2016-09-12 18:05:05 +0800

    problem 88 approaching

---
+++ problem-088.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def product_sum(k):
+    init_list = [1]*k
+
+
+def main():
+    print "Result: {}".format()
+
+if __name__ == "__main__":
+    from timeit import default_timer
+    start_time = default_timer()
+    main()
+    end_time = default_timer()
+    print "Time used(s): {}".format(end_time-start_time)
