4bf5067fe3d64ed99e740e53a1443916709c24f6
Ethan <konmyn@163.com>
2016-09-13 18:05:14 +0800

    problem 88 continuing

--- problem-088.py
+++ problem-088.py
@@ -1,12 +1,42 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-def product_sum(k):
-    init_list = [1]*k
+def prime_list(upper_limit):
+    primes = [2]
+    if not isinstance(upper_limit, int):
+        return []
+    if upper_limit<2:
+        return []
+    n = 3
+    _is_prime = True
+    while n<=upper_limit:
+        for i in primes:
+            if n%i==0:
+                _is_prime = False
+                break
+        if _is_prime:
+            primes.append(n)
+        n += 2
+        _is_prime = True
+    return primes
 
+def factor_list(number, primes):
+    i = 0
+    prime_factors = []
+    while primes[i]<=number:
+        if number%primes[i]==0:
+            prime_factors.append(primes[i])
+            number /= primes[i]
+            continue
+        i += 1
+    return prime_factors
 
 def main():
-    print "Result: {}".format()
+    k = 323434
+    upper_limit = k
+    primes = prime_list(upper_limit)
+    factors = factor_list(k, primes)
+    print "Result: {}".format(factors)
 
 if __name__ == "__main__":
     from timeit import default_timer
