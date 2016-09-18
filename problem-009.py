3dcf1018ddfcf3cd9da694db75d8261f7d0b9157
Ethan <konmyn@163.com>
2016-09-18 18:06:35 +0800

    problem 9 added.

---
+++ problem-009.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+# source from problem 39
+def is_right_angle_triangle(low, medium, high):
+    if pow(low, 2) + pow(medium, 2) == pow(high, 2):
+        return True
+    return False
+
+
+def right_angle_triangle_list(primeter):
+    # a<=b<=c, a+b+c=primeter
+    a = 1
+    b = 1
+    sum_ab = a + b
+    c = primeter - sum_ab
+    stop_point = primeter / 3
+    while c > stop_point:
+        while a <= sum_ab / 2:
+            b = sum_ab - a
+            if is_right_angle_triangle(a, b, c):
+                count = [a, b, c]
+                c = stop_point
+                break
+            a += 1
+        c -= 1
+        a = 1
+        sum_ab = primeter - c
+    return count
+
+
+def main():
+    sum_abc = 1000
+    _abc = right_angle_triangle_list(sum_abc)
+    product = reduce(lambda x, y: x * y, _abc, 1)
+    print "Result: {} * {} * {} = {}".format(_abc[0], _abc[1], _abc[2], product)
+
+
+if __name__ == "__main__":
+    from timeit import default_timer
+    start_time = default_timer()
+    main()
+    end_time = default_timer()
+    print "Time used(s): {}".format(end_time - start_time)
