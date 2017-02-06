7d937deb8fcf8a9870ab572182ba10c1ea6cddd2
Ethan <konmyn@163.com>
2017-02-06 15:28:42 +0800

    forget to add p01

--- problem-001.py
+++ problem-001.py
@@ -2,25 +2,57 @@
 # -*- coding: utf-8 -*-
 
 
-def main():
-    n = 1
-    target_list = []
-    while n < 1000:
-        if not n % 3:
-            target_list.append(n)
-        elif not n % 5:
-            target_list.append(n)
-        n += 1
-    print "Result: {}".format(sum(target_list))
+from timeit import default_timer
+
+
+# class as decorator
+class runTime(object):
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self):
+        print "Running {}".format(self.func.__name__)
+        start_time = default_timer()
+        self.func()
+        end_time = default_timer()
+        print "Time used(s): {}".format(end_time-start_time)
+
+# function as decorator
+def runtime(func):
+    def new_func():
+        print "Running {}".format(new_func.__name__)
+        start_time = default_timer()
+        func()
+        end_time = default_timer()
+        print "Time used(s): {}".format(end_time-start_time)
+    new_func.__name__ = func.__name__
+    return new_func
+
+uplimit = 10**7
+
+@runTime
+# annotation can be inserted between decorator and its function.
+# this method time cost should be O(n).
+def brute_force_method():
+    global uplimit
+    total = 0
+    for n in xrange(1, uplimit):
+        if n%3 == 0 or n%5 == 0:
+            total += n
+    print "Result: {}".format(total)
+
+@runTime
+# this method time cost should be O(1).
+def formula_method():
+    global uplimit
+    count_3 = uplimit//3
+    count_5 = uplimit//5
+    count_15 = uplimit//15
+    total = (count_3*(count_3+1)*3 + count_5*(count_5+1)*5
+             - count_15*(count_15+1)*15)//2
+    print "Result: {}".format(total)
 
 
 if __name__ == "__main__":
-    from timeit import default_timer
-    start_time = default_timer()
-    main()
-    end_time = default_timer()
-    print "Time used(s): {}".format(end_time - start_time)
-
-if False:
-    # one line solution below:
-    print sum([x for x in xrange(1000) if not x % 3 or not x % 5])
+    brute_force_method()
+    formula_method()
