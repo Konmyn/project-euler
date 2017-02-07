f9af1ad9b615cd9de9ae06d62b9d8b047eed1c9e
Ethan <konmyn@163.com>
2017-02-07 10:03:59 +0800

    finds mistake in p01, and make repeated thing as one function.

--- problem-001.py
+++ problem-001.py
@@ -18,15 +18,18 @@ def brute_force_method():
             total += n
     print "Result: {}".format(total)
 
+# return the sum of numbers '<=' limit that are divisible by number.
+def sum_num(limit, number):
+    limit //= number
+    return number*limit*(limit+1) // 2
+
 @runTime
 # this method time cost should be O(1).
+# see Inclusion–exclusion principle on Wiki.
 def formula_method():
     global uplimit
-    count_3 = uplimit//3
-    count_5 = uplimit//5
-    count_15 = uplimit//15
-    total = (count_3*(count_3+1)*3 + count_5*(count_5+1)*5
-             - count_15*(count_15+1)*15)//2
+    total = sum_num(uplimit-1, 3) + sum_num(uplimit-1, 5) -\
+            sum_num(uplimit-1, 3*5)
     print "Result: {}".format(total)
 
 
