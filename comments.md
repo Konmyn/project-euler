22b4f1d3c828423e23bdc57f85978fdb12bb547d
Ethan <konmyn@163.com>
2017-03-13 15:32:12 +0800

    somehow solve p357

--- comments.md
+++ comments.md
@@ -3,3 +3,7 @@
 p079:
 Some kind of cheating, I do not like this problem.
 Please solve it by hand!
+p357:
+BruteForce() is not acceptable when L excced 1000000.
+newBruteForce() cost 36-49 seconds to get answer.
+Is there any good solution?
--- problem-357.py
+++ problem-357.py
@@ -7,37 +7,35 @@ from tools.common import is_prime, prime_sieve
 
 
 @runTime
-def bruteForce(L=100000001):
-    targets = [2]
+def bruteForce(L=101):
+    targets = [1, 2]
     primes = set([2, 3])
     for n in xrange(4, L, 2):
         if is_prime(n+1):
             primes.add(n+1)
-            for i in (2, int(n**0.5)):
+            for i in xrange(2, int(n**0.5)+1):
                 if n%i == 0 and i+n/i not in primes:
                     break
             else:
                 targets.append(n)
-    print targets
     print "Result: {}".format(sum(targets))
 
 @runTime
-def newBruteForce(L=100000001):
+def newBruteForce(L=101):
     primes = prime_sieve(L)
     pset = set(primes)
     targets = []
     for p in primes:
         t = p-1
-        for n in range(2, int(t**0.5)):
+        for n in xrange(2, int(t**0.5)+1):
             if t%n==0 and n+t/n not in pset:
                 break
         else:
             targets.append(t)
-    print targets
     print "Result: {}".format(sum(targets))
 
 
 if __name__ == "__main__":
-    L = 10001
-    bruteForce(L)
+    L = 100000001
+    # bruteForce(L)
     newBruteForce(L)
